ed2c6561d3679ffb6ce2b1560e837ad46c63d733
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-06-29 10:57:27 -0300

    chore: update shell scripts

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-mkdir -p build && cd build
-conan install .. -s compiler.libcxx=libstdc++11 --build=missing \
-  -o is-wire:build_benchmarks=True -o is-wire:build_tests=True
-conan build ..
+conan build . \
+    --build=missing \
+    --output-folder=build/ \
+    -s build_type=Release \
+    -s compiler.libcxx=libstdc++11 \
+    -o is-wire/*:build_tests=True \
+    -o is-wire/*:build_benchmarks=True
